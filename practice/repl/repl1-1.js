c0449de539dec212b17bb16e675333e625baef19
tinabeattysmith <tinatest2020@gmail.com>
2020-09-16 23:07:42 -0400

    9/16 EOD update

--- practice/repl/repl1-1.js
+++ practice/repl/repl1-1.js
@@ -1,12 +1,15 @@
 /*
 !Repl 1.1 code
 */
-  function stepTwo()  {
-    for(let num =0; num < 11; num ++) {
-      console.log(num);
-        if (num % 2 === 0)
-          console.log(`the number is even`);
+
+for(let num =0; num < 11; num ++)
+{
+  console.log(num);
+  function oddEven () 
+    {
+      if (num % 2 === 0)
+        console.log(`the number is even`);
         else console.log(`the number is odd`);
     }
-  }
-  stepTwo();
+  oddEven();
+}
