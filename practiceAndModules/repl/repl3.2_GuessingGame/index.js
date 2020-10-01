8cf187ee924493e4cf75053f3f494fc848824589
tinabeattysmith <tinatest2020@gmail.com>
2020-10-01 14:45:22 -0400

    10/1 MOD

--- practiceAndModules/repl/repl3.2_GuessingGame/index.js
+++ practiceAndModules/repl/repl3.2_GuessingGame/index.js
@@ -1,16 +1,9 @@
 let rNum; // randomingly generated number
 let uNum; // user's guess
 let aCount = 1; // attempt count
+let min = 1;
+let max = 10;
 
-let body = document.getElementsByTagName('body');
-let btnSubmit = document.getElementById('submit');
-let randomNum = document.getElementById('randomNum');
-let userNum1 = document.getElementById('userNum1');
-let attemptCount1 = document.getElementById('attemptCount1');
-let userNum2 = document.getElementById('userNum2');
-let attemptCount2 = document.getElementById('attemptCount2');
-let userNum3 = document.getElementById('userNum3');
-let attemptCount3 = document.getElementById('attemptCount3');
 let winLose = document.getElementById('result');
 
 // add event listener to button 
@@ -21,46 +14,63 @@ function genNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
     };
 
-// invokes generate number function and passes min/max paramters.
-rNum = genNumber(1,10);
-//display the random number in DOM
-randomNum.innerText = (`Randomly generated number is ${rNum}`);
+// invokes generate number function, passes min/max parameters, stores the num, and prints to console.
+rNum = genNumber(min,max);
+console.log('Random Number: ',rNum);
 
 function play() {
+
     //display prompt to user and store input in variable
     uNum = prompt('Enter a number greater than 0 and less than 11');
-    console.log('1st number: ', uNum);
-    if(uNum == '') {
-        alert(`Uhmm...you forgot the number.`);
-        uNum = prompt('Enter a number greater than 0 and less than 11');
-    } else {
-        aCount = 1;
-         while(aCount < 3 ) {
-            if (uNum == rNum) {
-                alert(`${uNum} is correct.  You win`);
-                result.innerText = (`You win!`);
-                // add an alert to play again or quit????
-                break
-                } else if(uNum > rNum) {
-                    aCount++
-                    alert(`${uNum} is incorrect. Try a lower number.`);
-                    console.log("Count after high alert: " , aCount); 
-                    uNum = '';
-                    uNum = prompt('Enter a number greater than 0 and less than 11');
-                    console.log(uNum);
-                    } else if (uNum < rNum) {
-                        aCount++
-                        alert(`${uNum} is incorrect. Try a higher number.`);
-                        console.log("Count after low alert: ", aCount);
+    //console.log(`Selection #${aCount} : `, uNum);
+        //if guess is empty (click OK with input)
+            while(uNum == '') {
+                uNum = prompt('Uhmm...you forgot the number.  Please try again. Enter a number greater than 0 and less than 11');
+                };
+            //if guess less than min       
+            while(uNum < min) {
+                uNum = prompt(`The number must be greater than ${min}. Please try again.`);
+                };  
+             //if guess greater than max       
+             while(uNum < min) {
+                uNum = prompt(`The number must be less than ${max}. Please try again.`);
+                };    
+            
+            //if guess is not a number
+            let isNumber = Number.isInteger(uNum)
+            console.log(isNumber);
+                if(isNumber = 'false'){
+                    prompt('Hhmmm...that is not a number.  Please try again.  Enter a number greater than 0 and less than 11'); 
+                } ;
+    
+        // if count less than 3 and guess == random: alert win, return, display result in DOM
+        // if count less than 3 and guess != random: alert incorrect, # too high/low, reprompt for input, increment counter
+        for (; aCount < 3 ; aCount++) {
+            if (uNum === rNum) { // guess is correct
+            alert(`${uNum} is correct.  You win`);
+            result.innerText = (`${uNum} is correct.  You win`);
+            // add an alert to play again or quit????
+            return
+            } else if(uNum > rNum) { // guess too high
+                        alert(`${uNum} is too high. Try again.`);
                         uNum = '';
-                        uNum = prompt('Enter a number greater than 0 and less than 11');
-                        console.log(uNum);
-                        //!this is not working
-                    } else {
-                        alert('You lose!  Better luck next time.')
-                        result.innerText = ('You lose!  Better luck next time');
-                    } ;          
-        };
-        
-    };
+                        uNum = prompt('Enter a lower number');
+                    } else if (uNum < rNum) { // guess too low
+                        alert(`${uNum} is too low. Try again.`);
+                        uNum = '';
+                        uNum = prompt('Enter a higher number');
+                    };
+        } ;
+        //if count == 3 and guess != random: alert lose, return, and display result in DOM
+        //if count == 3 and guess == random: alert win, return, and display result in DOM
+    if(aCount == 3) {
+        if (uNum != rNum) {
+            alert('You lose!  Better luck next time.')
+            result.innerText = (`You lose. Better luck next time!`);
+            return
+        } else if (uNum == rNum);
+            alert(`${uNum} is correct.  You win`)
+            result.innerText = (`${uNum} is correct.  You win`);
+            return
+    }; 
 };
