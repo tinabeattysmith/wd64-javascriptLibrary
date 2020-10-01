223a8f09574feb97bbb7fa84678d993affc3e65a
tinabeattysmith <tinatest2020@gmail.com>
2020-10-01 01:08:09 -0400

    9/30 EOD

--- practiceAndModules/repl/repl3.2_GuessingGame/index.js
+++ practiceAndModules/repl/repl3.2_GuessingGame/index.js
@@ -1,14 +1,16 @@
-
-
 let rNum; // randomingly generated number
 let uNum; // user's guess
-let aCount; // attempt count
+let aCount = 1; // attempt count
 
 let body = document.getElementsByTagName('body');
 let btnSubmit = document.getElementById('submit');
 let randomNum = document.getElementById('randomNum');
-let userNum = document.getElementById('userNum');
-let attemptCount = document.getElementById('attemptCount');
+let userNum1 = document.getElementById('userNum1');
+let attemptCount1 = document.getElementById('attemptCount1');
+let userNum2 = document.getElementById('userNum2');
+let attemptCount2 = document.getElementById('attemptCount2');
+let userNum3 = document.getElementById('userNum3');
+let attemptCount3 = document.getElementById('attemptCount3');
 let winLose = document.getElementById('result');
 
 // add event listener to button 
@@ -21,49 +23,44 @@ function genNumber(min, max) {
 
 // invokes generate number function and passes min/max paramters.
 rNum = genNumber(1,10);
-//display random number in DOM
+//display the random number in DOM
 randomNum.innerText = (`Randomly generated number is ${rNum}`);
-//console.log(rNum);
 
 function play() {
-    //console.log("clicked");
-    //display prompt to user
-    uNum = parseInt(prompt('Enter a number greater than 0 and less than 11'));
-
-    //response validation
-    //empty //?null, NaN, outside of range -Comeback too
-    console.log('User#: ', uNum);
-        if(uNum = '') {
-            alert(`Uhmm...you forgot the number.`);
-            uNum = (prompt('Enter a number greater than 0 and less than 11'));
-            } else {
-            //display user number in DOM
-            userNum.innerText = (`Your first guess is ${uNum}`)
-            aCount=1;
-            console.log("Count", aCount);
-        //console.log('User#: ', uNum);
-            }; 
-        
-    //! Still need to loop when aCount < 4
-        if(uNum == rNum) {
-            alert("Correct!  You win");
+    //display prompt to user and store input in variable
+    uNum = prompt('Enter a number greater than 0 and less than 11');
+    console.log('1st number: ', uNum);
+    if(uNum == '') {
+        alert(`Uhmm...you forgot the number.`);
+        uNum = prompt('Enter a number greater than 0 and less than 11');
+    } else {
+        aCount = 1;
+         while(aCount < 3 ) {
+            if (uNum == rNum) {
+                alert(`${uNum} is correct.  You win`);
+                result.innerText = (`You win!`);
+                // add an alert to play again or quit????
+                break
+                } else if(uNum > rNum) {
+                    aCount++
+                    alert(`${uNum} is incorrect. Try a lower number.`);
+                    console.log("Count after high alert: " , aCount); 
+                    uNum = '';
+                    uNum = prompt('Enter a number greater than 0 and less than 11');
+                    console.log(uNum);
+                    } else if (uNum < rNum) {
+                        aCount++
+                        alert(`${uNum} is incorrect. Try a higher number.`);
+                        console.log("Count after low alert: ", aCount);
+                        uNum = '';
+                        uNum = prompt('Enter a number greater than 0 and less than 11');
+                        console.log(uNum);
+                        //!this is not working
+                    } else {
+                        alert('You lose!  Better luck next time.')
+                        result.innerText = ('You lose!  Better luck next time');
+                    } ;          
         };
-        //! need to end the game. 
-                    
-        /*} else if (uNum > rNum) {
-                alert("Incorrect. Try a lower number.");
-                aCount++
-                console.log("Count", aCount);
-                nuNum = prompt('Enter a number greater than 0 and less than 11');
-                
-            } else if (uNum > rNum) {
-                alert("Incorrect. Try a higher number.");
-                aCount++
-                console.log("Count", aCount);
-                uNum = prompt('Enter a number greater than 0 and less than 11');
-            }; /*else {
-                alert("you lose") 
-                //! need to end the game. 
-            };*/
-
-};  
+        
+    };
+};
