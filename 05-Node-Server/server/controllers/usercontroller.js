a2c148343cee64c176d2b482d1f7e9f1c888856d
tinabeattysmith <tinatest2020@gmail.com>
2020-10-15 14:44:02 -0400

    10/15 ready for Test Client JS

--- 05-Node-Server/server/controllers/usercontroller.js
+++ 05-Node-Server/server/controllers/usercontroller.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const sequelize = require('../db');
 const User = sequelize.import('../models/user');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+
 
 router.post('/createuser', function (req, res){
     const username = req.body.user.username;
@@ -11,7 +13,7 @@ router.post('/createuser', function (req, res){
     User.create
     ({
         username: username,
-        passwordhash: pass
+        passwordhash: bcrypt.hashSync(pass, 10)
 
     }).then(
         function createSuccess(user){
@@ -24,6 +26,31 @@ router.post('/createuser', function (req, res){
         },
         function createError(err){
             res.send(500, err.message)
+        }
+    );
+});
+
+router.post('/signin', function (req, res) {
+    User.findOne( {where: { username: req.body.user.username} } ).then(
+        function(user) {
+            if (user) {
+                bcrypt.compare(req.body.user.password, user.passwordhash, function (err, matches) {
+                    if (matches) {
+                        var token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: 60*60*24});
+                        res.json({
+                            user: user,
+                            message: "successfully authenticated",
+                            session: token
+                        });
+                    } else {
+                        res.status(502).send({error: "yoo failed, yo"});
+                    }
+                });
+            } else {
+                res.status(500).send({error: "failed to authenticate"});
+            }
+        }, function (err) {
+            res.status(501).send({error: "you failed, yo"})
         }
     );
 });
